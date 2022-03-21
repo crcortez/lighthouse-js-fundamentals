db8efab97398a6af03834456c7a0c6fed81e6f84
Chanella Cortez <chanellacortez@gmail.com>
2022-03-21 03:29:32 -0700

    Use a while loop to repeat a chorus line

--- lighthouse-js-fundamentals/tinkering.js
+++ lighthouse-js-fundamentals/tinkering.js
@@ -1,16 +1,13 @@
 // using function age for whichSchool, output is based on function (age)
 const whichSchool  = function (age) {
   // output for age less than 13
-  if (age <= 13) {
-    console.log("Elementary")
+  if (age < 13) {
+    return "Elementary School";
 // output for ages between 13-18
-  } else if (age > 13 && age < 18) {
-    console.log("Secondary School")
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School" 
 // output for ages over 18
   } else {
-    console.log("Lighthouse Labs")
+    return "Lighthouse Labs"
   }
 }
-
-console.log("I am 35. Which school should I go to");
-console.log(whichSchool(35));
