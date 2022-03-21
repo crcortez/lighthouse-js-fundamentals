4c31a80ae71b916f33e68672b1093816003271a2
Chanella Cortez <chanellacortez@gmail.com>
2022-03-20 21:10:59 -0700

    Add more conditional exmaples

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -1,12 +1,5 @@
-const raining = false;
-const cold = false;
+const temperature = -20
 
-if (raining) {
-  console.log("Don't forget your umbrealla!");
-  }
-
-if (cold) {
-  console.log("Make sure you pick out a scarf");
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
 }
-
-console.log("Now you're ready to go outside");
