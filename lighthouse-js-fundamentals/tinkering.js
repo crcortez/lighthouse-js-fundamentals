f6854e7908e1bd563b437faea3665b76721f7912
Chanella Cortez <chanellacortez@gmail.com>
2022-03-22 23:12:19 -0700

    test2

--- lighthouse-js-fundamentals/tinkering.js
+++ lighthouse-js-fundamentals/tinkering.js
@@ -1,6 +1,9 @@
-var cry = function sound(sob) {
-  var wah = "boohoo!"
-  return wah;
+var laugh = function(num) {
+  sound = "";
+  for (var i = 0; i < num; i++) {
+  sound += "ha";
+  }
+  return sound;
 }
 
-console.log(cry());
+console.log(laugh(3));
