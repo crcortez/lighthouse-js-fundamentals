0c0bed8ca80e682e8f1a83a1362d342927ed5eed
Chanella Cortez <chanellacortez@gmail.com>
2022-03-23 01:49:31 -0700

    age calculator challenge

---
+++ lighthouse-js-fundamentals/functions-age-calculator.js
@@ -0,0 +1,9 @@
+
+
+function ageCalculator(name, yearOfBirth, currentYear) {
+  return `${name} is ${currentYear - yearOfBirth} years old.`
+
+}
+
+console.log(ageCalculator("Miranda", 1983, 2022));
+console.log(ageCalculator("Ferdinand", 1988, 2022));
