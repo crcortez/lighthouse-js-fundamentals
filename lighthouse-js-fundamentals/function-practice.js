95cd3ae8c8c3c795581f3495266818c99ebd24b4
Chanella Cortez <chanellacortez@gmail.com>
2022-03-22 22:25:43 -0700

    function practice

--- lighthouse-js-fundamentals/function-practice.js
+++ lighthouse-js-fundamentals/function-practice.js
@@ -1,10 +1,16 @@
-// function declaration that takes in two arguments: a function for displaying
-// a message, along with a name of a movie
-function movies(messageFunction, name) {
-  messageFunction(name);
+// function expression catSays
+var catSays = function(max) {
+  var catMessage = "";
+  for (var i = 0; i < max; i++) {
+    catMessage += "meow ";
+  }
+  return catMessage;
+};
+
+// function declaration helloCat accepting a callback
+function helloCat(callbackFunc) {
+  return "Hello " + callbackFunc(3);
 }
 
-// call the movies function, pass in the function and name of movie
-movies(function displayFavorite(movieName) {
-  console.log("My favorite movie is " + movieName);
-}, "Finding Nemo");
+// pass in catSays as a callback function
+helloCat(catSays);
